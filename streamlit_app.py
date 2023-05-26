09702e94983c12f69554e4bdbf4a747108690e44
zanottidba <134708940+zanottidba@users.noreply.github.com>
2023-05-26 19:44:03 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,20 +26,19 @@ streamlit.dataframe(fruits_to_show)
 
 # New API to Display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+   fruit_choice = streamlit.text_input('What fruit would you like information about?')
+if not fruit_choice:
+   streamlit.error("Please select a fruit to get information.")
+else:
+   fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+   streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+-------
 #streamlit.text(fruityvice_response.json())
-
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-
-# The requirements.txt file you just added to your project tells Streamlit what libraries you plan to use in your project so it can add them in advance
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()
