9699bfea818246804c0db6dad1f481f1247eba44
zanottidba <134708940+zanottidba@users.noreply.github.com>
2023-05-26 19:23:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,4 +44,5 @@ my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
-
+ # need to be fixed
+ my_cur.execute("insert into fruit_load_list values ('from streamlit')")
