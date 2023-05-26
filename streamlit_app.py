17c3e713651ead6b07e61083735f243d11cee670
zanottidba <134708940+zanottidba@users.noreply.github.com>
2023-05-26 14:29:20 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit
-streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Cheese')
-streamlit.text('Goiabada')
-streamlit.text('Flat White')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
