3ad1130515658aa456021b5ea4b22a01076327f0
zanottidba <134708940+zanottidba@users.noreply.github.com>
2023-05-26 19:15:24 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,3 +36,12 @@ streamlit.dataframe(fruityvice_normalized)
 
 # The requirements.txt file you just added to your project tells Streamlit what libraries you plan to use in your project so it can add them in advance
 import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
